c1f7dbdfa090b9712fe56cde27d40ba53196dc60
liwenhua <liwenhua@liwenhuadeMacBook-Pro.local>
2018-12-02 16:21:27 +0800

    add

--- README.md
+++ README.md
@@ -24,3 +24,13 @@ http://localhost:9999/
 pm2 stop vue-todo
 
 test local history
+
+# 正式环境配置
+1，进入远程服务器目录
+2，clone代码
+3，进入项目目录，npm run i
+4，npm run build
+5,pm2 start pm2.yml --env production启动服务
+
+此时不能通过域名访问，只能通过ip访问，因为域名只能监听80端口, 需要通过nginx转发到nodejs的服务
+
--- build/webpack.config.server.js
+++ build/webpack.config.server.js
@@ -10,54 +10,54 @@ let config
 const isDev = process.env.NODE_ENV === 'development'
 
 const plugins = [
-    new ExtractPlugin('styles.[contentHash:8].css'),
-    new webpack.DefinePlugin({
-        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
-        'process.env.VUE_ENV': '"server"'
-    })
+  new ExtractPlugin('styles.[contentHash:8].css'),
+  new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+    'process.env.VUE_ENV': '"server"'
+  })
 ]
 
 if (isDev) {
-    plugins.push(new VueServerPlugin())  // 引入此插件不会生成js文件，会生成一个json文件
+  plugins.push(new VueServerPlugin()) // 引入此插件不会生成js文件，会生成一个json文件
 }
 
 config = merge(baseConfig, {
-    target: 'node',
-    entry: path.join(__dirname, '../client/server-entry.js'),
-    devtool: 'source-map',
-    output: {
-        libraryTarget: 'commonjs2',   // 打包后的是module.exports
-        filename: 'server-entry.js',
-        path: path.join(__dirname, '../server-build')
-    },
-    externals: Object.keys(require('../package.json').dependencies),  // 声明不要打包的文件
-    module: {
-        rules: [
+  target: 'node',
+  entry: path.join(__dirname, '../client/server-entry.js'),
+  devtool: 'source-map',
+  output: {
+    libraryTarget: 'commonjs2', // 打包后的是module.exports
+    filename: 'server-entry.js',
+    path: path.join(__dirname, '../server-build')
+  },
+  externals: Object.keys(require('../package.json').dependencies), // 声明不要打包的文件
+  module: {
+    rules: [
+      {
+        test: /\.styl/,
+        use: ExtractPlugin.extract({
+          fallback: 'vue-style-loader',
+          use: [
+            'css-loader',
             {
-              test: /\.styl/,
-              use: ExtractPlugin.extract({
-                fallback: 'vue-style-loader',
-                use: [
-                  'css-loader',
-                  {
-                    loader: 'postcss-loader',
-                    options: {
-                      sourceMap: true,
-                    }
-                  },
-                  'stylus-loader'
-                ]
-              })
-            }
-        ]
-    },
-    plugins
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            'stylus-loader'
+          ]
+        })
+      }
+    ]
+  },
+  plugins
 })
 
 config.resolve = {
-    alias: {
-        'model': path.join(__dirname, '../client/model/server-model.js')
-    }
+  alias: {
+    'model': path.join(__dirname, '../client/model/server-model.js')
+  }
 }
 
 module.exports = config
